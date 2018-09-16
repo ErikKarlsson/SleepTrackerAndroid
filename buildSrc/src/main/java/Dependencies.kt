7829a978411bb4f022c84ed946cadfad7f75f0c0
Erik Karlsson <erik.karlsson.flash@gmail.com>
2018-09-16 15:41:20 +0200

    Update to Android Studio 3.2.0 RC3

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     val version_code = 1
     val version_name = "1.0"
 
-    val android_gradle_plugin = "3.2.0-beta05"
+    val android_gradle_plugin = "3.2.0-rc03"
     val arch_core = "1.1.0"
     val atsl_rules = "1.0.2"
     val atsl_runner = "1.0.2"
@@ -27,7 +27,7 @@ object Versions {
     val hamcrest = "1.3"
     val junit = "4.12"
     val junit5_plugin = "1.0.32"
-    val kotlin = "1.2.50"
+    val kotlin = "1.2.51"
     val leakcanary = "1.5.4"
     val lifecycle = "1.1.1"
     val mockito = "2.18.3"
