cf1bd9dee7ef7ed26a276a673911f1440115ba07
Erik Karlsson <erik.karlsson.flash@gmail.com>
2018-09-25 23:22:16 +0200

    Fix not all sleep within date range selected when filtering

--- app/src/main/java/net/erikkarlsson/simplesleeptracker/data/statistics/StatisticsDao.kt
+++ app/src/main/java/net/erikkarlsson/simplesleeptracker/data/statistics/StatisticsDao.kt
@@ -14,39 +14,39 @@ import net.erikkarlsson.simplesleeptracker.domain.entity.SleepCountYearMonth
 @Dao
 interface StatisticsDao {
 
-    @Query("SELECT count(*) FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to)")
+    @Query("SELECT count(*) FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to)")
     fun getSleepCount(from: String, to: String): Flowable<Int>
 
-    @Query("SELECT ROUND(avg(hours),3) FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to)")
+    @Query("SELECT ROUND(avg(hours),3) FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to)")
     fun getAverageSleepInHours(from: String, to: String): Flowable<Float>
 
-    @Query("SELECT ROUND(avg(hours),3) as 'hours', strftime('%w', from_date) as 'day' FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to) GROUP BY strftime('%w', to_date)")
+    @Query("SELECT ROUND(avg(hours),3) as 'hours', strftime('%w', from_date) as 'day' FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to) GROUP BY strftime('%w', to_date)")
     fun getAverageSleepDurationDayOfWeek(from: String, to: String): Flowable<List<DayOfWeekHours>>
 
-    @Query("SELECT * FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to) AND hours = (SELECT max(hours) FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to)) ORDER BY datetime(to_date) ASC LIMIT 1")
+    @Query("SELECT * FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to) AND hours = (SELECT max(hours) FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to)) ORDER BY date(substr(to_date, 0, 11)) ASC LIMIT 1")
     fun getLongestSleep(from: String, to: String): Flowable<SleepEntity>
 
-    @Query("SELECT * FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to) AND hours = (SELECT min(hours) FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to)) ORDER BY datetime(to_date) ASC LIMIT 1")
+    @Query("SELECT * FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to) AND hours = (SELECT min(hours) FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to)) ORDER BY date(substr(to_date, 0, 11)) ASC LIMIT 1")
     fun getShortestSleep(from: String, to: String): Flowable<SleepEntity>
 
-    @Query("SELECT avg(from_date_midnight_offset_seconds) as 'midnightOffsetInSeconds', strftime('%w', to_date) as 'day' FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to) GROUP BY strftime('%w', to_date)")
+    @Query("SELECT avg(from_date_midnight_offset_seconds) as 'midnightOffsetInSeconds', strftime('%w', to_date) as 'day' FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to) GROUP BY strftime('%w', to_date)")
     fun getAverageBedtimeMidnightOffsetInSecondsForDaysOfWeek(from: String, to: String): Flowable<List<DayOfWeekMidnightOffset>>
 
-    @Query("SELECT avg(from_date_midnight_offset_seconds) FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to)")
+    @Query("SELECT avg(from_date_midnight_offset_seconds) FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to)")
     fun getAverageBedtimeMidnightOffsetInSeconds(from: String, to: String): Flowable<Int>
 
-    @Query("SELECT avg(to_date_midnight_offset_seconds) as 'midnightOffsetInSeconds', strftime('%w', to_date) as 'day' FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to) GROUP BY strftime('%w', to_date)")
+    @Query("SELECT avg(to_date_midnight_offset_seconds) as 'midnightOffsetInSeconds', strftime('%w', to_date) as 'day' FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to) GROUP BY strftime('%w', to_date)")
     fun getAverageWakeUpMidnightOffsetInSecondsForDaysOfWeek(from: String, to: String): Flowable<List<DayOfWeekMidnightOffset>>
 
     @Query("SELECT count(*) as 'sleepCount', strftime('%Y', from_date) as 'yearString', strftime('%m', from_date) as 'monthString' FROM Sleep WHERE to_date != 0 GROUP BY strftime('%Y', from_date), strftime('%m', from_date)")
     fun getCountGroupedByYearMonth(): Flowable<List<SleepCountYearMonth>>
 
-    @Query("SELECT avg(to_date_midnight_offset_seconds) FROM Sleep WHERE to_date != 0 AND date(to_date) BETWEEN date(:from) AND date(:to)")
+    @Query("SELECT avg(to_date_midnight_offset_seconds) FROM Sleep WHERE to_date != 0 AND date(substr(to_date, 0, 11)) BETWEEN date(:from) AND date(:to)")
     fun getAverageWakeUpMidnightOffsetInSeconds(from: String, to: String): Flowable<Int>
 
-    @Query("SELECT * FROM Sleep WHERE to_date != 0 ORDER BY datetime(to_date) ASC LIMIT 1")
+    @Query("SELECT * FROM Sleep WHERE to_date != 0 ORDER BY date(substr(to_date, 0, 11)) ASC LIMIT 1")
     fun getYoungestSleep(): Flowable<SleepEntity>
 
-    @Query("SELECT * FROM Sleep WHERE to_date != 0 ORDER BY datetime(to_date) DESC LIMIT 1")
+    @Query("SELECT * FROM Sleep WHERE to_date != 0 ORDER BY date(substr(to_date, 0, 11)) DESC LIMIT 1")
     fun getOldestSleep(): Flowable<SleepEntity>
 }
