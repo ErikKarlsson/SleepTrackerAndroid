4e042b5b998d9e5776bf91e8c954d944e7b3ffc3
Erik Karlsson <erik.karlsson.flash@gmail.com>
2020-03-15 18:56:08 +0100

    Remove unused methods

--- libraries/core/src/main/java/net/erikkarlsson/simplesleeptracker/core/util/RxExt.kt
+++ libraries/core/src/main/java/net/erikkarlsson/simplesleeptracker/core/util/RxExt.kt
@@ -8,21 +8,6 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import java.util.concurrent.TimeUnit
 
-/**
- * The results of scanMap are only aggregations of the previous and next item,
- * whereas [Observable.scan] accumulates aggregations of all items over time.
- *
- * @param initialValue the initial value with type of upstream observable item.
- * @param biFunc the aggregation function that is invoked with the previous and next item.
- *
- */
-fun <T, R> Observable<T>.scanMap(initialValue: T, biFunc: (T, T) -> R): Observable<R> {
-    return this.startWith(initialValue)
-        .buffer(2, 1)
-        .filter { it.size >= 2 }
-        .map { biFunc.invoke(it[0], it[1]) }
-}
-
 fun View.clicksThrottle(disposables: CompositeDisposable, action: () -> Unit) {
     this.clicks()
             .throttleFirst(500, TimeUnit.MILLISECONDS)
