146019b466280e2800aa8a90acbafe797e818e36
Erik Karlsson <erik.karlsson@tradera.com>
2020-08-23 14:14:50 +0200

    Update Android Studio 4.2 Canary 7

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@ object Config {
 }
 
 object Versions {
-    val android_gradle_plugin = "4.1.0-rc01"
+    val android_gradle_plugin = "4.2.0-alpha07"
     val arch_core = "2.1.0"
     val atsl_rules = "1.2.0"
     val atsl_runner = "1.2.0"
